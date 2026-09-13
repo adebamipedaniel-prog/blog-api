5b8e9df6f9b645a59033b4c3e7ea4a193e059878
Daniel <adebamipedaniel@gmail.com>
2026-09-13 19:48:43 +0200

    Added timestamps to the user schema in models/User.js to track when users are created and updated.

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { timestamps: true }
+);
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
